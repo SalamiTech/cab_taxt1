cbe12f759354a3b50fe3e09aaadeab4bf296c663
DESMOND2716 <desmondnelson908@gmail.com>
2022-08-26 06:38:15 +0000

    fixed all errors

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cab_taxt1/screen/mainpage.dart';
 import 'package:cab_taxt1/firebase_options.dart';
-import 'package:flutterfire_cli/flutterfire_cli.dart';
 
-Future<void> main() async {
+void main() async {
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(MyApp());
+    options: DefaultFirebaseOptions.currentPlatform,);
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screen/mainpage.dart
+++ lib/screen/mainpage.dart
@@ -14,6 +14,7 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         title: Text('Main Page'),
       ),
+      body: const Center(child:Text('Main Page'))
     );
   }
 }
