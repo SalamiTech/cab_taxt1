2e65937af23a34827e023de85349b091299c874a
SalamiTech <salamigideon01@gmail.com>
2022-09-05 13:59:59 +0000

    Login Page

--- lib/screen/loginpage.dart
+++ lib/screen/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/material/material_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -72,9 +73,11 @@ class LoginPage extends StatelessWidget {
             ),
           )
 
-
-
+          ,MaterialButton (onPressed:(){},
+              child: Text('Login'),
+          )
         ],
+
       ),
     );
   }
